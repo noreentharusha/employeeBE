df32272672eb7252379090a0043f94065ead7572
noreens <noreen.subasinghe@ecologital.com>
2023-10-12 14:35:39 +0530

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 /// DATABASE CONNECTION
 mongoose.connect(
-    "mongodb+srv://Noreen:Tharusha@123@cluster0.nd3emdc.mongodb.net/employeeDB?retryWrites=true&w=majority",
+    "mongodb+srv://Noreen:Tharusha@123@cluster0.nd3emdc.mongodb.net/",
     { useNewUrlParser: true }
 );
 
